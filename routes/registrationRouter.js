eba65f5d68a3c2d78c34a993c31a68c7ab59cbcd
vagadani <44646908+vagadani@users.noreply.github.com>
2019-05-22 14:49:22 +0530

    Update registrationRouter.js

--- routes/registrationRouter.js
+++ routes/registrationRouter.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var registrationController = require('../controllers/registrationController');
+var registrationController = require('../controllers/registrationController.js');
 var rc = new registrationController();
 
 router.post('/', rc.create.bind(rc));
