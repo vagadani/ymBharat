2a5aabf72b5d1b6885fe5b9bd5d8ad7dbc3d81b7
vagadani <44646908+vagadani@users.noreply.github.com>
2019-05-22 12:00:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+var app = express();
+var config = require('./config/config.json');
+var portListen = config.listen;
+
+app.use(bodyparser.json());
+app.use(function (req, res, next) {
+    console.log(req.url)
+    if (req.url.substr(-1) === '/') {
+        return res.send({
+            message: "Welcome!!"
+        });
+    }
+    next();
+});
+
+
+app.use('/v1', require('./routes'));
+app.listen(portListen);
