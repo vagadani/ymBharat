31ebc46a0fe52e3a2ab0d1f95d6965b10fe58375
Naveen <naveen.vagadani@kaaratech.com>
2019-05-24 14:34:38 +0530

    changes

--- models/registrationModel.js
+++ models/registrationModel.js
@@ -9,7 +9,7 @@ function RegistrationModel() {
 RegistrationModel.prototype.create = function (req, callback) {
     db.query("INSERT INTO users (name,username,password,email,logintype,fid,mobileno) VALUES" + "(" + "'" + req.body.name + "','" + req.body.username + "','" + req.body.password + "','" + req.body.email + "','" + req.body.logintype + "','" + req.body.fid + "','" + req.body.mobileno + "')", function (err, dbData) {
         //console.log(err, dbData);
-        callback(err,{message:"Record Inserted successfully"});
+        callback(err,{message:"Record Inserted successfully!!!"});
     })
 };
 module.exports = RegistrationModel;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.send("This is YMB version one");
+    res.send("This is YMB version one!!!");
 })
 router.use('/registration', require('./registrationRouter'));
 
