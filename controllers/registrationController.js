657010c80ae9ae73b629c46bda9c3661425e202b
Naveen <naveen.vagadani@kaaratech.com>
2019-05-24 14:07:52 +0530

    changes

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,15 +1,15 @@
-/*var registrationModel = require('../models/registrationModel.js');
-var rm;*/
+var registrationModel = require('../models/registrationModel.js');
+var rm;
 
 function RegistrationController() {
    // rm = new registrationModel();
 }
 
 RegistrationController.prototype.create = function (req, res) {
-    res.status(200).json({message:"Data from controller"});
-   /* rm.create(req, function (error, data) {
+    console.log("Data from controller");
+    rm.create(req, function (error, data) {
         res.status(200).json(data);
-    })*/
+    })
 }
 
 
